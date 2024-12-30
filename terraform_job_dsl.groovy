d7cd28f22ff2f20cf5f26fbfba207812fd2d2ea4
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-12-30 10:45:35 +0530

    Create terraform_job_dsl.groovy

---
+++ terraform_job_dsl.groovy
@@ -0,0 +1,52 @@
+pipelineJob('terraform-aws-network') {
+    description('Pipeline for managing AWS Network Infrastructure using Terraform')
+
+    parameters {
+        choiceParam('ACTION', ['apply', 'destroy'], 'Choose whether to apply or destroy the infrastructure')
+        stringParam('GIT_URL', 'https://github.com/komalthakur853/AWS-Network.git', 'Git repository URL')
+        stringParam('BRANCH', '*/main', 'Git branch to checkout')
+        stringParam('TERRAFORM_DIR', '.', 'Directory containing Terraform files')
+        stringParam('AWS_REGION', 'us-east-2', 'AWS region to target')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('${GIT_URL}')
+                    }
+                    branch('${BRANCH}')
+                    extensions {
+                        relativeTargetDirectory('AWS-Network')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+
+    wrappers {
+        credentialsBinding {
+            string('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID')
+            string('AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY')
+        }
+    }
+
+    environmentVariables {
+        env('AWS_DEFAULT_REGION', '${AWS_REGION}')
+        env('TERRAFORM_DIR', '${TERRAFORM_DIR}')
+    }
+
+    properties {
+        disableConcurrentBuilds()
+        buildDiscarder {
+            strategy {
+                logRotator {
+                    numToKeepStr('10')
+                    daysToKeepStr('30')
+                }
+            }
+        }
+    }
+}
