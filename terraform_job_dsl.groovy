caf155311defc7e9df3e4fc087edafc910db65be
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-12-30 13:34:36 +0530

    Update terraform_job_dsl.groovy

--- terraform_job_dsl.groovy
+++ terraform_job_dsl.groovy
@@ -2,7 +2,6 @@ pipelineJob('Sprint-4-Terraform') {
     description('Pipeline for managing AWS Network Infrastructure using Terraform')
     parameters {
         choiceParam('ACTION', ['apply', 'destroy'], 'Choose whether to apply or destroy the infrastructure')
-        stringParam('GIT_URL', 'https://github.com/komalthakur853/Sprint-4-Terraform.git', 'Git repository URL')
         stringParam('BRANCH', 'main', 'Git branch to checkout')
         stringParam('TERRAFORM_DIR', '.', 'Directory containing Terraform files')
         stringParam('AWS_REGION', 'ap-south-1', 'AWS region to target')
@@ -12,7 +11,7 @@ pipelineJob('Sprint-4-Terraform') {
             scm {
                 git {
                     remote {
-                        url('${GIT_URL}')
+                        url('https://github.com/komalthakur853/Sprint-4-Terraform.git')
                     }
                     branch('${BRANCH}')
                     extensions {
