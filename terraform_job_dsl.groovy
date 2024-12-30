fd513701452c77aa7b85476522056dbb3c2d290c
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-12-30 12:54:13 +0530

    Update terraform_job_dsl.groovy

--- terraform_job_dsl.groovy
+++ terraform_job_dsl.groovy
@@ -4,7 +4,7 @@ pipelineJob('terraform-aws-network') {
     parameters {
         choiceParam('ACTION', ['apply', 'destroy'], 'Choose whether to apply or destroy the infrastructure')
         stringParam('GIT_URL', 'https://github.com/komalthakur853/Sprint-4-Terraform.git', 'Git repository URL')
-        stringParam('BRANCH', '*/main', 'Git branch to checkout')
+        stringParam('BRANCH', 'main', 'Git branch to checkout')
         stringParam('TERRAFORM_DIR', '.', 'Directory containing Terraform files')
         stringParam('AWS_REGION', 'ap-south-1', 'AWS region to target')
     }
@@ -26,14 +26,9 @@ pipelineJob('terraform-aws-network') {
         }
     }
 
-    wrappers {
-        credentialsBinding {
-            string('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID')
-            string('AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY')
-        }
-    }
-
     environmentVariables {
+        env('AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID') // Injecting AWS Access Key ID
+        env('AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY') // Injecting AWS Secret Access Key
         env('AWS_DEFAULT_REGION', '${AWS_REGION}')
         env('TERRAFORM_DIR', '${TERRAFORM_DIR}')
     }
